d852c5e86d0e97e478d9080ca7127650975b4c47
fh <francois.hernandez.fh@gmail.com>
2024-10-07 12:16:52 +0200

    route switching works, need to find a way to get locale for mdx based content

--- src/components/blog/Posts.tsx
+++ src/components/blog/Posts.tsx
@@ -5,13 +5,15 @@ import styles from '@/components/blog/Posts.module.scss';
 interface PostsProps {
     range?: [number] | [number, number];
     columns?: '1' | '2' | '3';
+    locale: string;
 }
 
 export function Posts({
     range,
-    columns = '1'
+    columns = '1',
+    locale ='en'
 }: PostsProps) {
-    let allBlogs = getPosts(['src', 'app', '[locale]', 'blog', 'posts', 'en']);
+    let allBlogs = getPosts(['src', 'app', '[locale]', 'blog', 'posts', locale]);
 
     const sortedBlogs = allBlogs.sort((a, b) => {
         return new Date(b.metadata.publishedAt).getTime() - new Date(a.metadata.publishedAt).getTime();

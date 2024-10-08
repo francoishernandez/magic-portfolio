1620a09aae35223b7549d257233a0dd9ef04ac99
fh <francois.hernandez.fh@gmail.com>
2024-10-08 14:18:42 +0200

    set localePrefix 'as-needed', better single locale support

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -7,7 +7,9 @@ export const routing = defineRouting({
   locales: ['en'],
  
   // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: 'en',
+
+  localePrefix: 'as-needed'
 });
  
 export type Locale = (typeof routing.locales)[number];
